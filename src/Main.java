582cf1f043a7ab2b5d274130f06fa1ddbb6c891b
Sean Dunn <smd6336@rit.edu>
2018-05-04 18:04:01 -0400

    Added code to the menu and added a class to hold static traversal functions.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main{
+    //Menu function and menu options
     public static void displayMenu(){
         System.out.println("Please choose an option.");
         System.out.println("0. Quit");
@@ -12,6 +13,26 @@ public class Main{
         System.out.print("> ");
     }
 
+    public static void createGraph(){
+
+    }
+
+    public static void editGraph(){
+
+    }
+
+    public static void addNode(){
+
+    }
+
+    public static void removeNode(){
+
+    }
+
+    public static void traversalMenu(){
+        System.out.println("\nWhich traversal option would you like?");
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         boolean done = false;
@@ -20,23 +41,29 @@ public class Main{
 
         while(!done){
             displayMenu();
-            int option = scan.nextInt();
+            int option = scan.nextInt(); //need to implement checks for if the user inputs a type that's not an int.
 
             switch(option){
                 case 0:
                     done = true;
                     break;
                 case 1:
+                    createGraph();
                     break;
                 case 2:
+                    editGraph();
                     break;
                 case 3:
+                    addNode();
                     break;
                 case 4:
+                    removeNode();
                     break;
                 case 5:
+                    traversalMenu();
                     break;
                 default:
+                    System.err.println("Please enter a valid option.");
                     break;
             }
         }
---
+++ src/Traverse.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Traverse{
+    public static void traverseDepthFirst(ArrayList<Node> graph){
+
+    }
+
+    public static void traverseBreadthFirst(ArrayList<Node> graph){
+
+    }
+
+    public static void traverseDijkstra(ArrayList<Node> graph){
+
+    }
+
+    public static void traverseAStar(ArrayList<Node> graph){
+
+    }
+}
